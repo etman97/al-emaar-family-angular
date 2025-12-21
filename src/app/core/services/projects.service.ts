6af9cbe19da4ea36377e8b3dc275c01f561c4e5a
M.Osman <M.Osman@MOSMAN-IT>
2025-12-21 09:34:59 +0200

    add projects without images

--- src/app/core/services/projects.service.ts
+++ src/app/core/services/projects.service.ts
@@ -353,8 +353,553 @@ export class ProjectsService {
         "assets/NARJIS Heights/nargish-3.jpg"
       ],
       image: "assets/project.png"
+    },
+    {
+      "id": "project12",
+      "category": "ongoing",
+      "name": {
+        "en": "Al Quds 1 Twin Tower",
+        "ar": "القدس 1 برج التوأم"
+      },
+      "description": {
+        "en": "",
+        "ar": ""
+      },
+      "type": {
+        "en": "Commercial",
+        "ar": "تجاري"
+      },
+      "location": {
+        "en": "Al-Quds District",
+        "ar": "حي القدس"
+      },
+      "locationUrl": "",
+      "area": "6563",
+      "stats": {
+        "offices": 27,
+        "parkings": 0,
+        "villas": 0,
+        "models": 0,
+        "units": 0,
+        "exhibitionsShowrooms": 5,
+        "floors": 0
+      },
+      "Units": [
+        { "unitId": 1, "name": { "en": "Exhibition 1", "ar": "معرض 1" }, "area": "324", "floor": "0" },
+        { "unitId": 2, "name": { "en": "Exhibition 2", "ar": "معرض 2" }, "area": "346", "floor": "0" },
+        { "unitId": 3, "name": { "en": "Exhibition 3", "ar": "معرض 3" }, "area": "123", "floor": "0" },
+        { "unitId": 4, "name": { "en": "Exhibition 4", "ar": "معرض 4" }, "area": "115", "floor": "0" },
+        { "unitId": 5, "name": { "en": "Exhibition 5", "ar": "معرض 5" }, "area": "163", "floor": "0" },
+        {
+          "unitId": 6,
+          "name": {
+            "en": "Office 1",
+            "ar": "مكتب 1"
+          },
+          "area": "387",
+          "floor": "1"
+        },
+        {
+          "unitId": 7,
+          "name": {
+            "en": "Office 2",
+            "ar": "مكتب 2"
+          },
+          "area": "359",
+          "floor": "1"
+        },
+        {
+          "unitId": 8,
+          "name": {
+            "en": "Office 3",
+            "ar": "مكتب 3"
+          },
+          "area": "128",
+          "floor": "1"
+        },
+        {
+          "unitId": 9,
+          "name": {
+            "en": "Office 4",
+            "ar": "مكتب 4"
+          },
+          "area": "125",
+          "floor": "1"
+        },
+        {
+          "unitId": 10,
+          "name": {
+            "en": "Office 5",
+            "ar": "مكتب 5"
+          },
+          "area": "154",
+          "floor": "1"
+        },
+        {
+          "unitId": 11,
+          "name": {
+            "en": "Office 6",
+            "ar": "مكتب 6"
+          },
+          "area": "147",
+          "floor": "1"
+        },
+        {
+          "unitId": 12,
+          "name": {
+            "en": "Office 7",
+            "ar": "مكتب 7"
+          },
+          "area": "407",
+          "floor": "2"
+        },
+        {
+          "unitId": 13,
+          "name": {
+            "en": "Office 8",
+            "ar": "مكتب 8"
+          },
+          "area": "360",
+          "floor": "2"
+        },
+        {
+          "unitId": 14,
+          "name": {
+            "en": "Office 9",
+            "ar": "مكتب 9"
+          },
+          "area": "125",
+          "floor": "2"
+        },
+        {
+          "unitId": 15,
+          "name": {
+            "en": "Office 10",
+            "ar": "مكتب 10"
+          },
+          "area": "135",
+          "floor": "2"
+        },
+        {
+          "unitId": 16,
+          "name": {
+            "en": "Office 11",
+            "ar": "مكتب 11"
+          },
+          "area": "161",
+          "floor": "2"
+        },
+        {
+          "unitId": 17,
+          "name": {
+            "en": "Office 12",
+            "ar": "مكتب 12"
+          },
+          "area": "145",
+          "floor": "2"
+        },
+        {
+          "unitId": 18,
+          "name": {
+            "en": "Villa 18",
+            "ar": "فيلا 18"
+          },
+          "area": "280",
+          "floor": "0"
+        },
+        {
+          "unitId": 19,
+          "name": {
+            "en": "Office 13",
+            "ar": "مكتب 13"
+          },
+          "area": "236",
+          "floor": "3"
+        },
+        {
+          "unitId": 20,
+          "name": {
+            "en": "Office 14",
+            "ar": "مكتب 14"
+          },
+          "area": "237",
+          "floor": "3"
+        },
+        {
+          "unitId": 21,
+          "name": {
+            "en": "Office 15",
+            "ar": "مكتب 15"
+          },
+          "area": "125",
+          "floor": "3"
+        },
+        {
+          "unitId": 22,
+          "name": {
+            "en": "Office 16",
+            "ar": "مكتب 16"
+          },
+          "area": "141",
+          "floor": "3"
+        },
+        {
+          "unitId": 23,
+          "name": {
+            "en": "Office 17",
+            "ar": "مكتب 17"
+          },
+          "area": "165",
+          "floor": "3"
+        },
+        {
+          "unitId": 24,
+          "name": {
+            "en": "Office 18",
+            "ar": "مكتب 18"
+          },
+          "area": "144",
+          "floor": "3"
+        },
+        {
+          "unitId": 25,
+          "name": {
+            "en": "Office 19",
+            "ar": "مكتب 19"
+          },
+          "area": "85",
+          "floor": "4"
+        },
+        {
+          "unitId": 26,
+          "name": {
+            "en": "Office 20",
+            "ar": "مكتب 20"
+          },
+          "area": "83",
+          "floor": "4"
+        },
+        {
+          "unitId": 27,
+          "name": {
+            "en": "Office 21",
+            "ar": "مكتب 21"
+          },
+          "area": "125",
+          "floor": "4"
+        },
+        {
+          "unitId": 28,
+          "name": {
+            "en": "Office 22",
+            "ar": "مكتب 22"
+          },
+          "area": "147",
+          "floor": "4"
+        },
+        {
+          "unitId": 29,
+          "name": {
+            "en": "Office 23",
+            "ar": "مكتب 23"
+          },
+          "area": "165",
+          "floor": "4"
+        },
+        {
+          "unitId": 30,
+          "name": {
+            "en": "Office 24",
+            "ar": "مكتب 24"
+          },
+          "area": "144",
+          "floor": "4"
+        },
+        {
+          "unitId": 31,
+          "name": {
+            "en": "Office 25",
+            "ar": "مكتب 25"
+          },
+          "area": "85",
+          "floor": "5"
+        },
+        {
+          "unitId": 32,
+          "name": {
+            "en": "Office 26",
+            "ar": "مكتب 26"
+          },
+          "area": "83",
+          "floor": "5"
+        },
+        {
+          "unitId": 33,
+          "name": {
+            "en": "Office 27",
+            "ar": "مكتب 27"
+          },
+          "area": "128",
+          "floor": "5"
+        },
+        {
+          "unitId": 34,
+          "name": {
+            "en": "Annex Office 28",
+            "ar": "مكتب 28 ملحق"
+          },
+          "area": "153",
+          "floor": "0"
+        },
+        {
+          "unitId": 35,
+          "name": {
+            "en": "Annex Office 29",
+            "ar": "مكتب 29 ملحق"
+          },
+          "area": "167",
+          "floor": "0"
+        },
+        {
+          "unitId": 36,
+          "name": {
+            "en": "Annex Office 30",
+            "ar": "مكتب 30 ملحق"
+          },
+          "area": "146",
+          "floor": "0"
+        },
+        {
+          "unitId": 37,
+          "name": {
+            "en": "Annex Office 31",
+            "ar": "مكتب 31 ملحق"
+          },
+          "area": "154",
+          "floor": "0"
+        },
+        {
+          "unitId": 38,
+          "name": {
+            "en": "Annex Office 32",
+            "ar": "مكتب 32 ملحق"
+          },
+          "area": "134",
+          "floor": "0"
+        }
+      ],
+      "images": [],
+      "image": ""
+    },
+    {
+      id: "project13",
+      category: "ongoing",
+      name: { en: "Al Masif Neo", ar: "المصيف الجديد" },
+      description: {
+        en: "",
+        ar: ""
+      },
+      type: { en: "Administrative", ar: "إداري" },
+      location: { en: "Al-Masif District", ar: "حي المصيف" },
+      locationUrl: "",
+      area: "3647",
+      stats: { offices: 21, parkings: 115, villas: 0, models: 0, units: 0, exhibitionsShowrooms: 0, floors: 0 },
+      "Units": [
+        {
+          "unitId": 1,
+          "name": {
+            "en": "Office 01",
+            "ar": "مكتب 01"
+          },
+          "area": "155",
+          "floor": "0"
+        },
+        {
+          "unitId": 2,
+          "name": {
+            "en": "Office 02",
+            "ar": "مكتب 02"
+          },
+          "area": "190",
+          "floor": "0"
+        },
+        {
+          "unitId": 3,
+          "name": {
+            "en": "Office 03",
+            "ar": "مكتب 03"
+          },
+          "area": "165",
+          "floor": "0"
+        },
+        {
+          "unitId": 4,
+          "name": {
+            "en": "Office 04",
+            "ar": "مكتب 04"
+          },
+          "area": "215",
+          "floor": "0"
+        },
+        {
+          "unitId": 5,
+          "name": {
+            "en": "Office 05",
+            "ar": "مكتب 05"
+          },
+          "area": "202",
+          "floor": "0"
+        },
+        {
+          "unitId": 6,
+          "name": {
+            "en": "Office 06",
+            "ar": "مكتب 06"
+          },
+          "area": "147",
+          "floor": "0"
+        },
+        {
+          "unitId": 7,
+          "name": {
+            "en": "Office 07",
+            "ar": "مكتب 07"
+          },
+          "area": "174",
+          "floor": "0"
+        },
+        {
+          "unitId": 8,
+          "name": {
+            "en": "Office 08",
+            "ar": "مكتب 08"
+          },
+          "area": "168",
+          "floor": "0"
+        },
+        {
+          "unitId": 9,
+          "name": {
+            "en": "Office 09",
+            "ar": "مكتب 09"
+          },
+          "area": "177",
+          "floor": "1"
+        },
+        {
+          "unitId": 10,
+          "name": {
+            "en": "Office 10",
+            "ar": "مكتب 10"
+          },
+          "area": "190",
+          "floor": "1"
+        },
+        {
+          "unitId": 11,
+          "name": {
+            "en": "Office 11",
+            "ar": "مكتب 11"
+          },
+          "area": "165",
+          "floor": "1"
+        },
+        {
+          "unitId": 12,
+          "name": {
+            "en": "Office 12",
+            "ar": "مكتب 12"
+          },
+          "area": "215",
+          "floor": "1"
+        },
+        {
+          "unitId": 13,
+          "name": {
+            "en": "Office 13",
+            "ar": "مكتب 13"
+          },
+          "area": "221",
+          "floor": "1"
+        },
+        {
+          "unitId": 14,
+          "name": {
+            "en": "Office 14",
+            "ar": "مكتب 14"
+          },
+          "area": "147",
+          "floor": "1"
+        },
+        {
+          "unitId": 15,
+          "name": {
+            "en": "Office 15",
+            "ar": "مكتب 15"
+          },
+          "area": "174",
+          "floor": "1"
+        },
+        {
+          "unitId": 16,
+          "name": {
+            "en": "Office 16",
+            "ar": "مكتب 16"
+          },
+          "area": "183",
+          "floor": "1"
+        },
+        {
+          "unitId": 17,
+          "name": {
+            "en": "Annex Office 17",
+            "ar": "مكتب 17 الملحق"
+          },
+          "area": "135",
+          "floor": "Annex"
+        },
+        {
+          "unitId": 18,
+          "name": {
+            "en": "Annex Office 18",
+            "ar": "مكتب 18 الملحق"
+          },
+          "area": "174",
+          "floor": "Annex"
+        },
+        {
+          "unitId": 19,
+          "name": {
+            "en": "Annex Office 19",
+            "ar": "مكتب 19 الملحق"
+          },
+          "area": "162",
+          "floor": "Annex"
+        },
+        {
+          "unitId": 20,
+          "name": {
+            "en": "Annex Office 20",
+            "ar": "مكتب 20 الملحق"
+          },
+          "area": "144",
+          "floor": "Annex"
+        },
+        {
+          "unitId": 21,
+          "name": {
+            "en": "Annex Office 21",
+            ar: "مكتب 21 الملحق"
+          },
+          "area": "137",
+          "floor": "Annex"
+        }
+      ],
+      images: [],
+      image: ""
     }
   ];
+
 
   setSelectedProject(project: Project): void {
     this.selectedProject = project;
